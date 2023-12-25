a3bc2973ebfbfb88331d171659631d91da45e82c
PrashantMaurya252 <mauryaprashant252@gmail.com>
2023-12-25 08:27:40 +0530

    adding new Book

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export const PORT=5555;
 
-export const MONGODBURL=`mongodb+srv://${process.env.mongoUser}:${process.env.mongoPassword}@cluster0.7mk2cyi.mongodb.net/${process.env.mongoDataBase}?retryWrites=true&w=majority`;
+// export const MONGODBURL=`mongodb+srv://${process.env.mongoUser}:${process.env.mongoPassword}@cluster0.7mk2cyi.mongodb.net/${process.env.mongoDataBase}?retryWrites=true&w=majority`;
+export const MONGODBURL=`mongodb+srv://bookstore:book123@cluster0.7mk2cyi.mongodb.net/?retryWrites=true&w=majority`;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express, { response } from "express";
-import { PORT,MONGODBURL } from "./config";
+import { PORT,MONGODBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
@@ -9,7 +10,30 @@ app.get('/',(request,response)=>{
     return response.status(234).send('Welcome to MERN stack tutorial')
 })
 
+// Route for save a book
 
+app.post('/books',async (request,response)=>{
+    try {
+        if(!request.body.title || request.body.author || request.body.publishYear){
+            return response.status(400).send({
+                message:'Send all required field:title,author,publishYear'
+            })
+        };
+        const newBook={
+            title:request.body.title,
+            author:request.body.author,
+            publishYear:request.body.publishYear
+        }
+
+        const book=await Book.create(newBook);
+        return response.status(201).send(book)
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message:error.message})
+        
+    }
+
+})
 
 mongoose.connect(MONGODBURL).then(()=>{
     console.log('App to connected to database');
