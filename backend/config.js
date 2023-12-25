eb231b89f0725b05195363431ba77fcdc76d87aa
PrashantMaurya252 <mauryaprashant252@gmail.com>
2023-12-25 13:10:19 +0530

    setting tailwind and react router

--- backend/config.js
+++ backend/config.js
@@ -3,8 +3,5 @@ import mongoose from 'mongoose';
 dotenv.config();
 
 export const PORT=5555;
-console.log(process.env.mongoUser);
-console.log(process.env.mongoPassword)
-
 export const MONGODBURL=`mongodb+srv://${process.env.mongoUser}:${process.env.mongoPassword}@cluster0.7mk2cyi.mongodb.net/?retryWrites=true&w=majority`;
-console.log(MONGODBURL)
+
--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,26 @@ import { PORT,MONGODBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import router from "./routes/booksRoute.js";
+import cors from 'cors'
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json())
+
+// Middleware for handling CORS policy
+
+//option1:Allow all origin with default of cors
+// app.use(cors());
+// option2:Allow custom origin
+
+app.use(
+    cors({
+        origin:"http://localhost:5555",
+        methods:['GET','POST','PUT','DELETE'],
+        allowedHeaders:['Content-Type']
+    })
+)
 
 app.get('/',(request,response)=>{
     console.log(request);
